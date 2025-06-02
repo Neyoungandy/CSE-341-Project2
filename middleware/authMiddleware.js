f90171df6877d4b27fe237ee6db3c7e539a87b93
neyoungandy <>
2025-06-02 01:54:15 +0100

    few adjustments

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const ensureAuthenticated = (req, res, next) => {
     console.log("Session Data:", req.session);
-    console.log("Authenticated:", req.isAuthenticated());
-    if (req.isAuthenticated()) {
+    console.log("User Data:", req.user); // Ensure user is attached
+    if (req.session.passport && req.session.passport.user) { 
         return next();
     }
     res.status(401).json({ error: "Unauthorized, please log in via GitHub." });
